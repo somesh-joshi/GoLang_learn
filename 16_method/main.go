063789551e89a12a5ff600106ab370310641af46
Somesh <someshjoshi890@gmail.com>
2022-12-30 09:08:37 +0530

    jq

---
+++ 16_method/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// it is alternative of classes
+	// there is no inheritance, no super, no parent, no child
+
+	somesh := Person{
+		Name:   "Somesh",
+		Age:    25,
+		Email:  "abc@gmail.com",
+		Status: true,
+	}
+
+	somesh.greet()
+}
+
+type Person struct {
+	Name   string
+	Age    int
+	Email  string
+	Status bool
+}
+
+func (p Person) greet() {
+	fmt.Println("Hello", p.Name)
+}
+
+// to make method exportable, we need to capitalize the first letter of the method name
+// to make struct exportable, we need to capitalize the first letter of the struct name
+// to make variable exportable, we need to capitalize the first letter of the variable name
