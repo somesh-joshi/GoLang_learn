817782b2eaf8b98a4e8db1c2afc258bb98b561ad
Somesh <someshjoshi890@gmail.com>
2023-01-04 19:09:18 +0530

    sdfhjdksf

--- 22_mongoapi/main.go
+++ 22_mongoapi/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/somesh-joshi/mongoapi/router"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println("MongoDB API")
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listening at port 4000 ...")
 }
