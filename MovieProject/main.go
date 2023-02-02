71b3208cf5c4b0f070c18853a20b3bc3bbf7d14c
Somesh <someshjoshi890@gmail.com>
2023-02-02 14:56:56 +0530

    finish

--- MovieProject/main.go
+++ MovieProject/main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"net/http"
+	"io/ioutil"
+	"strconv"
+	"strings"
 
-	"github.com/somesh-joshi/MovieProject/routers"
+	"github.com/somesh-joshi/MovieProject/db"
+	"github.com/somesh-joshi/MovieProject/models/person"
 )
 
+var coll = db.Db.Collection("file")
+
 func main() {
-	r := routers.Router()
-	fmt.Println("Server is getting started...")
-	log.Fatal(http.ListenAndServe(":4000", r))
+	data, err := ioutil.ReadFile("file.txt")
+	if err != nil {
+		panic(err)
+	}
+	B := strings.Split(string(data), ",")
+	datainterface := make([]interface{}, 0)
+	for i := 0; i < len(B); i += 4 {
+		date, _ := strconv.Atoi(B[i+2])
+		person := person.Person{
+			Title: B[i+0],
+			Name:  B[i+1],
+			Age:   date,
+			DoB:   B[i+3],
+		}
+		datainterface = append(datainterface, person)
+	}
+	result, err := coll.InsertMany(context.TODO(), datainterface)
+	if err != nil {
+		panic(err)
+	}
+	for _, v := range result.InsertedIDs {
+		fmt.Println(v)
+	}
 }
--- MovieProject/models/person/person.go
+++ MovieProject/models/person/person.go
@@ -6,8 +6,8 @@ import (
 
 type Person struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title string             `json:"title,omitempty" validate:"required"`
-	Name  string             `json:"name,omitempty" validate:"required"`
-	Age   int                `json:"age,omitempty" validate:"required,numeric"`
-	DoB   string             `json:"dob,omitempty" validate:"required"`
+	Title string             `json:"title,omitempty"`
+	Name  string             `json:"name,omitempty"`
+	Age   int                `json:"age,omitempty"`
+	DoB   string             `json:"dob,omitempty"`
 }
